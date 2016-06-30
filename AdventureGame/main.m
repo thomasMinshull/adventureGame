b6c041819334d27ad39d2b512f8d51532ed0718e
Thomas Minshull <thomasminshull@gmail.com>
2016-06-29 18:55:13 -0700

    initial commit

---
+++ AdventureGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AdventureGame
+//
+//  Created by thomas minshull on 2016-06-29.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
